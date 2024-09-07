a1fb4c6caba175757a7f05505cd51a8a1412e5b5
kevserna <serna.kevn@gmail.com>
2024-09-07 17:17:41 -0400

    open and close modal

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,15 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalClose = document.querySelector("#modal-close");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+modalClose.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
